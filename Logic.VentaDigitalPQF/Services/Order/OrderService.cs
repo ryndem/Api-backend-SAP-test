d9178ee3794fb6df7e2cbbc4607cde54e793bcfb
any.arizmendi@gmail.com <ana.arizmendi@ryndem.mx>
2024-11-27 10:07:02 -0600

    Prueba

--- Logic.VentaDigitalPQF/Services/Order/OrderService.cs
+++ Logic.VentaDigitalPQF/Services/Order/OrderService.cs
@@ -70,7 +70,7 @@ namespace Logic.VentaDigitalPQF.Services.Order
                 new()
                 {
                     IdOrder = Guid.NewGuid(),
-                    Folio = "Folio1",
+                    Folio = "PruebaSAP",
                     IdfilePdf = Guid.NewGuid(),
                     RegistrationDate = DateTime.Now.AddDays(-30),
                     Items = 5,
